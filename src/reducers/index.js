3d77747570ff18f3c27d73b67e7cfa6f9a3593c8
Gabeh98 <bielhenry09@gmail.com>
2021-05-24 13:28:44 -0300

    Deixando codigo limpo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
+import { combineReducers } from "redux";
+import posts from "./posts";
 
 export const reducers = combineReducers({ posts });
